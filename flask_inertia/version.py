85c6165cd3dfe96575595694cc14531bd1e2724f
Alex Mohr <mohralexj@gmail.com>
2025-01-13 11:34:27 -0800

    use blueprint template folder for asset version

--- flask_inertia/version.py
+++ flask_inertia/version.py
@@ -33,14 +33,19 @@ Provide a method to calculate Flask assets version.
 import hashlib
 import os
 
-from flask import current_app
+from flask import current_app, request
 
 
 def get_asset_version() -> str:
     """Calculate asset version to allow Inertia to automatically make a full page visit in case of changes."""
+    template_folder = current_app.template_folder
+    if request.blueprint is not None:
+        blueprint = current_app.blueprints[request.blueprint]
+        if blueprint.template_folder is not None:
+            template_folder = blueprint.template_folder
     template_path = os.path.join(
         current_app.root_path,
-        current_app.template_folder,
+        template_folder,
         current_app.config["INERTIA_TEMPLATE"],
     )
     with open(template_path, "rb") as template_file:
